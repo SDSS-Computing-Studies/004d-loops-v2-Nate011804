ce5fc1a84f4f60513089a8359f03ca1490f36537
Nate011804 <71406872+Nate011804@users.noreply.github.com>
2020-10-13 11:34:15 -0700

    Update task3.py

--- task3.py
+++ task3.py
@@ -18,3 +18,10 @@ list of numbers
 """
 
 numList = (25, 8, 10, 11, 33, 30, 51, 75, 63, 14, 20, 99)
+ChickenTendies = (5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100)
+a = input( "number")
+a = int(a)
+
+answer = a / 5
+if answer == int(answer):
+     print("That number is divisible by 5")
